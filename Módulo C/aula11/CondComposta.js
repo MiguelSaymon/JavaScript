04770e3ff8142fd17a1e888e50d4a5c84024676a
MiguelSaymon <miguelsaimon09@gmail.com>
2025-04-05 16:20:26 -0300

    .

---
+++ Módulo C/aula11/CondComposta.js
@@ -0,0 +1,7 @@
+var idade = 18 // Digamos que o usuário colocou 18 anos
+
+if (idade < 18) { // Condição Composta
+    console.log(" Você está permitido a entrar")
+} else {
+    console.log('Você é de menor, não pode entrar')
+}
---
+++ Módulo C/aula11/CondSimples.js
@@ -0,0 +1,7 @@
+
+var velocidade = 60.5
+
+if (velocidade <= 50.5) { // Condição simples
+    console.log('Você passou acima da velocidade permitida. MULTADO')
+} 
+console.log('Lembre-se de usar o cinto de segurança')
