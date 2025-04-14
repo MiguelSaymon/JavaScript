a71872aa3f56777940351cd02ef7eb759b2e0394
MiguelSaymon <miguelsaimon09@gmail.com>
2025-04-14 17:42:55 -0300

    Create Repetições.js

---
+++ Módulo E/aula13/Repetições.js
@@ -0,0 +1,13 @@
+var contador = 1
+
+while (contador <= 10) { // Repetições Sequênciais
+    console.log('[OK]')
+    contador++
+}
+
+console.log('---------------------------//------------------------------')
+
+do {
+    console.log('[OK]')
+    contador++
+} while (contador <= 6)
