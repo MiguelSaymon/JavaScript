7e8a0245d220acd20e44529fd4653d5f813b662b
MiguelSaymon <miguelsaimon09@gmail.com>
2025-06-02 15:58:36 -0300

    Create FuncaoPt1.js

---
+++ Módulo F/aula16/FuncaoPt1.js
@@ -0,0 +1,18 @@
+function parOrImpar(numero){
+    if (numero%2 == 0) {
+        return 'par'
+    }else {
+        return 'impar'
+    }
+}
+
+let resultado = parOrImpar(17)
+console.log(resultado)
+
+console.log('---------//----------')
+
+function soma(n1, n2) {
+    return n1 + n2
+}
+
+console.log(soma(5, 50))
